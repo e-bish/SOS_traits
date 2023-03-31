15535268397a7722a6ffe1c3c6b66b449cd6e06d
Emily Bishop <ebish@uw.edu>
2023-03-31 16:22:41 -0700

    created first draft species count matrix

---
+++ analysis/fish/02_tidy_data.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(here)
+library(rfishbase)
+
+#Load data
+#load data (raw data downloaded/formatted in "import_data.R")
+net_2018.19 <- here::here("data","net_18.19.csv") %>% 
+  read_csv()
+
+net_2021 <- here::here("data","net_2021.csv") %>% 
+  read_csv()
+
+net_2022 <- here::here("data","net_2022.csv") %>% 
+  read_csv()
+
+################################################################################
+#prepare data for analysis
+net_tidy <- bind_rows(net_2018.19, net_2021, net_2022) %>% 
+  filter(!ipa == "Armored_2") %>% #remove second armored site from Titlow in 2021
+  mutate(ipa = replace(ipa, site == "TUR" & ipa == "Restored", "Natural")) %>% #no restoration at Turn Island
+  mutate(site_ipa = paste(site, ipa, sep = "_")) %>% 
+  mutate(date = make_date(year, month, day)) %>% 
+  mutate(species_count = as.numeric(species_count)) %>%
+  mutate(species_count = ifelse(is.na(org_type), 0, species_count)) %>% 
+  filter(org_type == "Fish") %>% #### this is where you lose zero counts, if those are needed later
+  rename(ComName = "species") %>% 
+  mutate(ComName = case_when(tax_group == "Salmon" ~ paste(ComName, "salmon"), 
+                             ComName == "Sand Sole" ~ "Pacific sand sole",
+                             ComName == "Herring" ~ "Pacific herring", 
+                             ComName == "Snake Prickleback" ~ "Pacific snake prickleback",
+                             ComName == "Speckled Sand Dab" ~ "Speckled sanddab",
+                             ComName == "Poacher" ~ "UnID Poacher",
+                             ComName == "Striped Perch" ~ "Striped Seaperch",
+                             ComName == "Staghorn Sculpin" ~ "Pacific staghorn sculpin",
+                             ComName == "Stickleback" ~ "Three-spined stickleback",
+                             ComName == "Tom Cod" ~ "Pacific tomcod",
+                             ComName == "Sand Lance" ~ "Pacific sand lance",
+                             ComName == "Pipefish" ~ "Bay pipefish",
+                             ComName == "Irish Lord" ~ "UnID Irish Lord",
+                             ComName == "Midshipman" ~ "Plainfin midshipman",
+                             ComName == "White Spotted Greenling" ~ "Whitespotted greenling",
+                             ComName == "Silver Spotted Sculpin" ~ "Silverspotted sculpin",
+                             ComName == "Tube Snout" ~ "Tube-snout",
+                             TRUE ~ ComName)) %>% 
+  mutate(ComName = case_when(ComName == "Steelhead salmon" ~ "Steelhead trout",
+                             ComName == "Cutthroat salmon" ~ "Cutthroat trout",
+                             TRUE ~ ComName)) %>% 
+  select(date, site_ipa, ComName, species_count) %>% 
+  arrange(ComName)
+
+spp_names <- net_tidy %>% 
+  distinct(ComName) %>% 
+  mutate(Species = NA)
+
+sci_names <- list()
+
+for (i in 1:nrow(comm_names)) {
+  sci_names[[i]] <- rfishbase::common_to_sci(spp_names[i,])
+  spp_names[i,2] <- ifelse(nrow(sci_names[[i]]) == 1, sci_names[[i]][[1]], NA) 
+}
+
+spp_names <- spp_names %>% 
+  mutate(Species = case_when(ComName == "Pacific herring" ~ "Clupea pallasii", 
+                           ComName == "Pacific Cod" ~ "Gadus macrocephalus",
+                           ComName == "Northern Anchovy" ~ "Engraulis mordax",
+                           ComName == "Striped Seaperch" ~ "Embiotoca lateralis",
+                           ComName == "Rock Sole" ~ "Lepidopsetta bilineata",
+                           ComName == "Steelhead trout" ~ "Oncorhynchus mykiss",
+                           ComName == "Cutthroat trout" ~ "Oncorhynchus clarkii",
+                           ComName == "Tube-snout" ~ "Aulorhynchus flavidus",
+                           TRUE ~ Species)) %>% 
+  arrange(ComName) %>% 
+  filter(!str_detect(ComName, 'UnID'))
+
+MaxN <- net_tidy %>% 
+  group_by(site_ipa, ComName) %>%
+  filter(species_count == max(species_count)) %>% 
+  ungroup() %>% 
+  rename(MaxN = "species_count") %>% 
+  select(!date) %>% 
+  distinct(site_ipa, ComName, MaxN)
+ 
+fish.MaxN <- MaxN %>% 
+  complete(site_ipa, ComName) %>% 
+  replace(is.na(.), 0) %>% 
+  pivot_wider(names_from = ComName, values_from = MaxN) %>% 
+  select(-contains("UnID"))
+  column_to_rownames(var="site_ipa") %>% 
+  as.matrix()
+
