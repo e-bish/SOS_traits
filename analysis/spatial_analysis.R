3003f2d6669b8db335946ba89bb1862e9d34730e
Emily Bishop <ebish@uw.edu>
2024-03-27 09:21:45 -0700

    updated order of HUCs to match sites

--- analysis/spatial_analysis.R
+++ analysis/spatial_analysis.R
@@ -8,16 +8,12 @@ library(terra)
 national_HU12 <- here("data","spatial","NWBD_HUC_12-Digit_Basins_of_WA","WBDHU12.shp") %>% 
   read_sf()
 
-shoreline <- here("data","spatial","shorezone_shoreline_only","shorezone_shoreline_only.shp") %>% 
-  read_sf() %>% 
-  st_transform(crs = 2927) 
-
 site_levels <- c("FAM", "TUR", "COR", "MA", "WA", "HO", "SHR", "DOK", "LL", "TL", "PR", "EDG")
 
 #GPS locations for our survey stations with each ipa
 SOS_sites <- here("data","spatial","reupdatingthearmoringgeodatabase", "Shoreline_armoring_shore_origin_sites_UTM.shp") %>% 
   read_sf() %>% #UTM zone 10 32610
-  st_transform(crs = 2927)  %>% #transform site coordinates into the same crs as the shoreline layer
+  st_transform(crs = 2927)  %>% #transform site coordinates into the same crs as the HUC layer
   mutate(site = case_when(Site_name == "Dockton Park" ~ "DOK",
                           Site_name == "Cornet Bay" ~ "COR",
                           Site_name == "Edgewater Beach" ~ "EDG",
@@ -30,7 +26,7 @@ SOS_sites <- here("data","spatial","reupdatingthearmoringgeodatabase", "Shorelin
                           Site_name == "Waterman Shoreline Preserve" ~ "WA" ,
                           Site_name == "Howarth Park" ~ "HO" ,
                           Site_name == "Maylor Point" ~ "MA"), .after = "Site_name") %>% 
-  mutate(site = factor(site, levels = site_levels))
+  mutate(site = factor(site, levels = site_levels)) 
 
 #create a buffer around sites that will overlap with the HUCs
 site_buffers <- st_buffer(SOS_sites, 1000)
@@ -40,46 +36,80 @@ land_HU12 <- national_HU12 %>%
   filter(!Name %in% c("Puget Sound", "Skagit Bay", "Haro Strait-Strait of Georgia", "South Puget Sound"))
 
 SOS_HUCS_all <- st_filter(land_HU12, site_buffers)
-#note this number matches the number of sites because while COR overlaps two HUCs, there are two sites on Vashon
+#note this number matches the number of sites because while there are two sites on Vashon, COR overlaps two HUCs
 
 SOS_HUCS <- SOS_HUCS_all %>% 
-  select(c(HUC12, Name, geometry)) 
+  select(c(HUC12, Name, geometry))  
+
+#create centroid points to make ordering N to S easier
+SOS_HUC_cents <- st_centroid(SOS_HUCS) %>% 
+  cbind(st_coordinates(.)) %>% 
+  st_drop_geometry() %>% 
+  mutate(Y = ifelse(grepl("Tacoma", Name), Y + 50000, Y)) %>% 
+  arrange(desc(Y))
+
+#reorder from north to south and add SOS site names
+SOS_HUCS <- SOS_HUCS %>% 
+  mutate(Name = factor(Name, levels = unique(SOS_HUC_cents$Name))) %>% 
+  arrange(Name)
 
 #### map it ####
 
-SOS_site_cents_sn$lon <- st_coordinates(SOS_site_cents_sn)[,1]
-SOS_site_cents_sn$lat <- st_coordinates(SOS_site_cents_sn)[,2]
+SOS_sites_transformed <- SOS_sites %>% 
+  st_zm() %>% 
+  st_transform(crs = 4326) %>% 
+  mutate(lon = st_coordinates(.)[,1],
+         lat = st_coordinates(.)[,2])
 
-leaflet(st_transform(SOS_site_cents_sn, crs = 4326)) %>%
+leaflet(SOS_sites_transformed) %>%
+  addCircleMarkers(lng = ~lon, lat = ~lat, color = "purple") %>% 
   addProviderTiles(providers$Esri.WorldGrayCanvas, group =  "Esri") %>%
   setView(lng =-122.420429, lat = 47.886010, zoom = 8) %>%
   addPolylines(data = st_transform(national_HU12, crs = 4326), popup = national_HU12$Name) %>%
-  addPolylines(data = st_transform(SOS_HUCS, crs = 4326), color = "red", label = ~Name) %>%
-  addCircleMarkers(lng = ~lon, lat = ~lat, color = "purple")
-
-####################
-#### format to add SOS sites ####
-# #create centroid points to make ordering N to S easier
-# SOS_HUC_cents <- st_centroid(SOS_HUCS) %>% 
-#   cbind(st_coordinates(.)) 
-# 
-# #this is only working in base r for some reason
-# SOS_HUC_cents <- SOS_HUC_cents[order(SOS_HUC_cents$Y, decreasing = T),]
-# 
-# #reorder from north to south and add SOS site names
-# SOS_HUCS_full <- SOS_HUCS %>% 
-#   slice(rep(1:n(), times = c(1,1,1,4,1,1,1,2)))  %>% #multiple sites in some HUC12s 
-#   mutate(Name = factor(Name, levels = unique(SOS_HUC_cents$Name))) %>% 
-#   arrange(Name) %>% 
-#   mutate(site = site_levels, .after = "Name")
+  addPolylines(data = st_transform(SOS_HUCS, crs = 4326), color = "red", label = ~Name)
 
 #### load C-CAP data ####
+#transform HUCs to the projection of the ccap data
+SOS_HUCS_proj <- st_transform(SOS_HUCS, crs = 5070)
+
+#load 30m resolution land cover data
+ccap_2016lc <- rast(here("data", "spatial", "2016_CCAP_Job987470", "2016_CCAP_J987470.tif"))
+
+#classify land cover data based on NOAA codes
+cover <- c("background", "unclassified", "high_intensity_developed",
+           "medium_intensity_developed", "low_intensity_developed",
+           "developed_open_space", "cultivated_land", "pasture.hay", 
+           "grassland", "deciduous_forest", "evergreen_forest",
+           "mixed_forest", "shrub.scrub", "palustrine_forested_wetland",
+           "palustrine_scrub.shrub_wetland", "palustrine_emergent_wetland",
+           "estuarine_forested_wetland", "estuarine_scrub.shrub_wetland",
+           "estuarine_emergent_wetland", "unconsolidated_shore", "bare_land",
+           "open_water", "palustrine_aquatic_bed", "estuarine_aquatic_bed","tundra", "snow.ice")
+
+levels(ccap_2016lc) <- data.frame(id=0:25, cover=cover)
+
+#extract land cover by HUCs
+start.time <- Sys.time()
+ccap_lc_HUCs <- terra::extract(ccap_2016lc, SOS_HUCS_proj) 
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time,2)
+time.taken # <5 seconds 
+
+#format land cover data for each HUC
+lc_table <- table(ccap_lc_HUCs) %>% 
+  as_tibble() %>% 
+  filter(!n == 0) %>% 
+  pivot_wider(names_from = cover, values_from = n, values_fill = 0) %>% 
+  column_to_rownames(var="ID") %>% 
+  mutate(rowsum16 = rowSums(.))
+
+#assign land cover values to proper HUCs
+env_raw_table <- SOS_HUCS_proj %>% 
+  st_drop_geometry() %>% 
+  bind_cols(lc_table)
 
 #load 1m resolution impervious surface data
 # ccap_2023imperv <- rast(here("data", "spatial", "wa_2021_ccap_v2_hires_impervious_20231119", "wa_2021_ccap_v2_hires_impervious_20231119.tif"))
-# 
-SOS_HUCS_proj<- st_transform(SOS_HUCS, crs = 5070)
-# 
 # #extract imperv by HUCs
 # start.time <- Sys.time()
 # ccap_imperv_HUCs <- terra::extract(ccap_2023imperv, SOS_HUCS_proj) #this takes forever
@@ -90,42 +120,51 @@ SOS_HUCS_proj<- st_transform(SOS_HUCS, crs = 5070)
 # save(imperv_table, file = here("data", "imperv.table.Rdata"))
 load(here("data", "imperv.table.Rdata")) #load the table instead of running the extract again
 
-env_table <- as_tibble(imperv_table) %>% 
+#reformat impervious surface data by HUC
+imperv_tibble <- as_tibble(imperv_table) %>% 
   pivot_wider(names_from = wa_2021_ccap_v2_hires_impervious_20231119, values_from = n) %>% 
-  rename(natural = "0", imperv = "1") %>% 
-  cbind(SOS_HUCS_proj) %>% 
-  select(!c(ID,geometry)) %>% 
-  select(HUC12, Name, natural, imperv) %>%
-  mutate(perc.imperv23 = round(100*(imperv/(natural + imperv)),2)) %>% 
-  mutate(perc.natural23 = 100 - perc.imperv23)
+  rename(natural23 = "0", imperv23 = "1") %>% 
+  select(!ID) %>% 
+  mutate(rowsum23 = rowSums(.)) 
 
-#load 30m resolution land cover data
-#load 1m resolution impervious surface data
-ccap_2016lc <- rast(here("data", "spatial", "2016_CCAP_Job987470", "2016_CCAP_J987470.tif"))
+#combine data from both sets of ccap data
+env_raw_table <-  bind_cols(env_raw_table, imperv_tibble)
 
-cover <- c("background", "unclassified", "high intensity developed",
-           "medium intensity developed", "low intensity developed",
-           "developed open space", "cultivated land", "pasture/hay", 
-           "grassland", "deciduous forest", "evergreen forest",
-           "mixed forest", "shrub/scrub", "palustrine forested wetland",
-           "palustrine scrub/shrub wetland", "palustrine emergent wetland",
-           "estuarine forested wetland", "estuarine scrub/shrub wetland",
-           "estuarine emergent wetland", "unconsolidated shore", "bare land",
-           "open water", "palustrine aquatic bed", "estuarine aquatic bed","tundra", "snow/ice")
+#add up the values for the two HUCs that Cornet spans
+Cornet_Hucs <- env_raw_table %>% 
+  filter(HUC12 ==  171100190101 | HUC12 == 171100190102) %>% 
+  summarise(across(where(is.numeric), sum)) %>% 
+  mutate(Name = "Cornet_HUC", .before = high_intensity_developed)
 
-levels(ccap_2016lc) <- data.frame(id=0:25, cover=cover)
+#remove HUC12s to combine data by site
+SOS_env_table <- env_raw_table %>% 
+  select(!HUC12) %>% 
+  bind_rows(Cornet_Hucs) %>% 
+  mutate(Name = factor(Name, levels = unique(SOS_HUC_cents$Name))) %>% 
+  arrange(Name)
 
-start.time <- Sys.time()
-ccap_lc_HUCs <- terra::extract(ccap_2016lc, SOS_HUCS_proj) 
-end.time <- Sys.time()
-time.taken <- round(end.time - start.time,2)
-time.taken # <5 seconds 
+#combine data in the correct order
+# SOS_env_table2 <- bind_rows(SOS_env_table[1:2,],
+#                            Cornet_Hucs,
+#                            SOS_env_table[5:8,], 
+#                            SOS_env_table[9,], 
+#                            SOS_env_table[9,], 
+#                            SOS_env_table[10:12,]) %>% 
+#   mutate(site = site_levels, .after = Name)
+#   
 
-lc_table <- table(ccap_lc_HUCs) %>% 
-  as_tibble() %>% 
-  filter(!n == 0) %>% 
-  pivot_wider(names_from = cover, values_from = n, values_fill = 0) %>% 
-  column_to_rownames(var="ID") %>% 
-  mutate(rowsum = rowSums(.))
+env_table <- SOS_env_table2 %>% 
+  rowwise() %>% 
+  mutate(perc.developed = sum(high_intensity_developed, medium_intensity_developed, low_intensity_developed, developed_open_space)/rowsum16,
+         perc.agriculture = sum(cultivated_land, pasture.hay)/rowsum16,
+         perc.forest = sum(deciduous_forest, evergreen_forest, mixed_forest)/rowsum16,
+         perc.shrub = sum(shrub.scrub)/rowsum16,
+         perc.wetland = sum(palustrine_forested_wetland, palustrine_scrub.shrub_wetland, 
+                            palustrine_emergent_wetland, estuarine_emergent_wetland)/rowsum16,
+         perc.bare_land = sum(bare_land)/rowsum16) %>%
+  mutate(perc.imperv23 = imperv23/(natural23 + imperv23)) %>% 
+  mutate(perc.natural23 = 1 - perc.imperv23) %>% 
+  select(!3:26)
+  
 
 
