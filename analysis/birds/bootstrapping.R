a486f0b6ea2e69d92a17b311a3ce3827d08c7a85
Emily Bishop <ebish@uw.edu>
2024-06-18 18:04:38 -0400

    updated boostrap with more iterations

--- analysis/birds/bootstrapping.R
+++ analysis/birds/bootstrapping.R
@@ -1,6 +1,7 @@
 library(here)
 library(tidyverse)
 library(mFD)
+library(ggridges)
 library(patchwork)
 
 #load tidy fish data frame created in 01_tidy_data
@@ -9,7 +10,7 @@ load(here("data", "bird.list.Rdata")) #object created in 02_create_matrices
 
 SOS_core_sites <- c("FAM", "TUR", "COR", "SHR", "DOK", "EDG")
 
-#bootstrap tidy dataframe
+#### bootstrap tidy dataframe ####
 
 expand_species <- birds_tidy %>% 
   expand(nesting(year, month, site, ipa), spp_code) %>% 
@@ -24,10 +25,9 @@ mat_to_boot <- birds_tidy %>%
   mutate(spp_sum = replace_na(spp_sum, 0)) %>% 
   arrange(spp_code)
 
-# boot_list <- lapply(1:20, function(i) mat_to_boot[sample(nrow(mat_to_boot)),])
-
+#bootstrap
 boot_list <- list()
-nboot <- 20
+nboot <- 999
 
 for (i in seq_len(nboot)) {
   boot_list[[i]] <- mat_to_boot[sample(seq_len(nrow(mat_to_boot)), nrow(mat_to_boot), replace = TRUE),]
@@ -76,8 +76,6 @@ trait_space <- space_quality$"details_fspaces"$"sp_pc_coord"
 
 #### calculate the functional indices ####
 alpha_indices <- list()
-# results_list <- vector("list", length = 5)
-# names(results_list) <- FD_indices
 FD_results <- data.frame()
 
 # with the mFD package
@@ -96,21 +94,19 @@ for (i in 1:length(boot_L)){
     as_tibble(rownames = "site") 
   
   FD_results <- rbind(FD_results, FD_results_df)
-  
-  # for (j in 1:length(FD_indices)){
-  #   results_list[[j]] <- rbind(results_list[[j]], FD_values_df[,c(1,j+1)])
-  # }
+
 }
 #the mFD package uses ape::pcoa() which automatically removes negative eigenvalues rather than applying a correction
 
-# FD_results_df <- map_df(results_list, ~as.data.frame(.x), .id="id")
+colnames(FD_results)[2:6] <- c("Species_Richness", "FDis", "FEve", "FRic", "FDiv")
 
-# names(FD_results[2:6]) <- c("Species_Richness", "FDis", "FEve", "FRic", "FDiv")
-library(ggridges)
+FD_results <- FD_results %>% 
+  mutate(region = ifelse(site %in% c("FAM", "TUR", "COR"), "North", "South"))
+
 plot_site_index <- function (index){
   ggplot(data = FD_results, aes(x = .data[[index]], 
-                                y = site, 
-                                color = site, fill = site)) +
+                                y = factor(site, levels = rev(SOS_core_sites)), 
+                                fill = region, color = region)) +
     geom_density_ridges() + 
     theme_classic() +
     xlab(index) +
