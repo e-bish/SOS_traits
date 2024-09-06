0f6bae3d865d54c2510ea7b6f5810c5eed39e189
Emily Bishop <ebish@uw.edu>
2024-09-05 17:01:30 -0700

    added a different way to calculate p-values

--- analysis/fish/06_compare_to_null.R
+++ analysis/fish/06_compare_to_null.R
@@ -46,7 +46,7 @@ FD_null_results <- FD_null_results %>%
     mutate(site = factor(site, levels = c("FAM", "TUR", "COR", "SHR", "DOK", "EDG"))) %>% 
     mutate(region = ifelse(site %in% c("FAM", "TUR", "COR"), "North", "South"), .after = site)
 
-save(FD_null_results, file = "data/FD_null_results.Rda")
+# save(FD_null_results, file = "data/FD_null_results.Rda")
 load("data/FD_null_results.Rda")  
 
 FD_null_summary <- FD_null_results %>% 
@@ -54,21 +54,21 @@ FD_null_summary <- FD_null_results %>%
   summarize(across(where(is.numeric), list(mean = mean, sd = sd)))
 
 ## use the equation in Zhang et al. to calculate SES in original dataset
-load("data/FD_results.Rda")  
-
-FD_means <- FD_results %>% 
-  group_by(site) %>% 
-  summarize(across(where(is.numeric), mean))
-
-SES_tab <- as.data.frame(FD_means$site)
-SES_tab[,2] <- (FD_means$Species_Richness - FD_null_summary$Species_Richness_mean) / FD_null_summary$Species_Richness_sd
-SES_tab[,3] <- (FD_means$FRic - FD_null_summary$FRic_mean) / FD_null_summary$FRic_sd
-SES_tab[,4] <- (FD_means$FEve - FD_null_summary$FEve_mean) / FD_null_summary$FEve_sd
-SES_tab[,5] <- (FD_means$FDiv - FD_null_summary$FDiv_mean) / FD_null_summary$FDiv_sd
-SES_tab[,6] <- (FD_means$FDis - FD_null_summary$FDis_mean) / FD_null_summary$FDis_sd
-
-names(SES_tab) <- names(FD_means[1:6])
- 
+# load("data/FD_results.Rda")  
+# 
+# FD_means <- FD_results %>% 
+#   group_by(site) %>% 
+#   summarize(across(where(is.numeric), mean))
+# 
+# SES_tab <- as.data.frame(FD_means$site)
+# SES_tab[,2] <- (FD_means$Species_Richness - FD_null_summary$Species_Richness_mean) / FD_null_summary$Species_Richness_sd
+# SES_tab[,3] <- (FD_means$FRic - FD_null_summary$FRic_mean) / FD_null_summary$FRic_sd
+# SES_tab[,4] <- (FD_means$FEve - FD_null_summary$FEve_mean) / FD_null_summary$FEve_sd
+# SES_tab[,5] <- (FD_means$FDiv - FD_null_summary$FDiv_mean) / FD_null_summary$FDiv_sd
+# SES_tab[,6] <- (FD_means$FDis - FD_null_summary$FDis_mean) / FD_null_summary$FDis_sd
+# 
+# names(SES_tab) <- names(FD_means[1:6])
+#  
 ## calculate SES for bootstrapped samples 
 load("data/FD_boot_results.Rda")  
 
@@ -85,41 +85,56 @@ SES_boot_tab[,6] <- (FD_boot_means$FDis - FD_null_summary$FDis_mean) / FD_null_s
 
 names(SES_boot_tab) <- names(FD_boot_means)
 
-SES_all <- full_join(SES_tab, SES_boot_tab, by = "site", suffix = c(".o", ".boot")) #.o for original
+# SES_all <- full_join(SES_tab, SES_boot_tab, by = "site", suffix = c(".o", ".boot")) #.o for original
 #Negative SES values indicated trait convergence, while positive values indicated trait divergence (Götzenberger et al., 2012).
 
 ## calculate significance with p-values
 
 SOS_core_sites <- c("FAM", "TUR", "COR", "SHR", "DOK", "EDG")
+# 
+# ggplot() + 
+#   geom_histogram(data = FD_boot_results, mapping = aes(Species_Richness)) +
+#   facet_wrap(~site)
 
-ggplot() + 
-  geom_histogram(data = FD_boot_results, mapping = aes(Species_Richness)) +
-  facet_wrap(~site)
+# calc_p_values <- function(HA_df) {
+# 
+#   p_tab <- as.data.frame(matrix(nrow = 6, ncol = 6))
+#   names(p_tab) <- names(FD_boot_means)
+#   p_tab[,1] <- FD_boot_means$site
+#   
+#   for(i in 1:length(SOS_core_sites)) {
+#     
+#     tmp_null <- FD_null_results %>% filter(site == SOS_core_sites[i])
+#     tmp_HA <- HA_df %>% filter(site == SOS_core_sites[i])
+# 
+#     p_tab[i,2] <- sum(tmp_null$Species_Richness >= tmp_HA$Species_Richness) / nrow(tmp_null)
+#     p_tab[i,3] <- sum(tmp_null$FRic >= tmp_HA$FRic) / nrow(tmp_null)
+#     p_tab[i,4] <- sum(tmp_null$FEve >= tmp_HA$FEve) / nrow(tmp_null)
+#     p_tab[i,5] <- sum(tmp_null$FDiv >= tmp_HA$FDiv) / nrow(tmp_null)
+#     p_tab[i,6] <- sum(tmp_null$FDis >= tmp_HA$FDis) / nrow(tmp_null)
+#   
+#   }
+#   
+#   return(p_tab)
+# }
 
-calc_p_values <- function(HA_df) {
+# P_vals <- calc_p_values(FD_means)
+# P_boot_vals <- calc_p_values(FD_boot_means)
 
-  p_tab <- as.data.frame(matrix(nrow = 6, ncol = 6))
-  names(p_tab) <- names(FD_boot_means)
-  p_tab[,1] <- FD_boot_means$site
-  
-  for(i in 1:length(SOS_core_sites)) {
-    
-    tmp_null <- FD_null_results %>% filter(site == SOS_core_sites[i])
-    tmp_HA <- HA_df %>% filter(site == SOS_core_sites[i])
 
-    p_tab[i,2] <- sum(tmp_null$Species_Richness >= tmp_HA$Species_Richness) / nrow(tmp_null)
-    p_tab[i,3] <- sum(tmp_null$FRic >= tmp_HA$FRic) / nrow(tmp_null)
-    p_tab[i,4] <- sum(tmp_null$FEve >= tmp_HA$FEve) / nrow(tmp_null)
-    p_tab[i,5] <- sum(tmp_null$FDiv >= tmp_HA$FDiv) / nrow(tmp_null)
-    p_tab[i,6] <- sum(tmp_null$FDis >= tmp_HA$FDis) / nrow(tmp_null)
-  
-  }
-  
-  return(p_tab)
-}
+#### make a new function that extracts the 25th and 975th value from each set
 
-P_vals <- calc_p_values(FD_means)
-P_boot_vals <- calc_p_values(FD_boot_means)
+FD_null_results %>% 
+  filter(site == "TUR") %>% 
+  arrange(FRic) %>% 
+  nth(25)
+
+FD_null_results %>% 
+  filter(site == "TUR") %>% 
+  arrange(FRic) %>% 
+  nth(975)
+
+########
 
 calc_SES_p_values <- function() {
   
